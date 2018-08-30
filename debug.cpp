100565646e628d06f2d8426cdf9e115161a6bae3
Karl Zylinski <karl@zylinski.se>
2018-08-31 00:08:39 +0200

    blubb

--- debug.cpp
+++ debug.cpp
@@ -1,14 +1,14 @@
 #include "debug.h"
 #include "renderer.h"
 
-static Renderer* G_renderer = nullptr;
+static Renderer* _renderer = nullptr;
 
 void debug_init(Renderer* r)
 {
-    G_renderer = r;
+    _renderer = r;
 }
 
 void debug_draw_mesh(const Vec3* vertices, unsigned num_vertices, const Color& color)
 {
-    G_renderer->draw_debug_mesh(vertices, num_vertices, color);
+    _renderer->draw_debug_mesh(vertices, num_vertices, color);
 }
