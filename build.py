bf3cc592b31d3b3989cee9522874d63f039d95db
Karl Zylinski <karl@zylinski.se>
2019-08-08 00:37:15 +0200

    build script

--- build.py
+++ build.py
@@ -15,8 +15,27 @@ for f in all_files:
     if f.endswith(".c"):
         to_compile.append(f)
 
-all_inputs = " ".join(to_compile)
-c = os.system("clang -c -std=c99 -Wall -Werror %s -g -DVK_USE_PLATFORM_XCB_KHR" % all_inputs)
+if not os.path.isdir("build"):
+    os.mkdir("build")
 
-if len(sys.argv) > 1 and sys.argv[1] == "run" and c == 0:
+built_objects = []
+
+for in_filename in to_compile:
+    object_filename = in_filename[0:-2] + ".o"
+    out_filename = "build/" + object_filename
+    compile_error = os.system("clang -c -std=c99 -Wall -Werror %s -o %s -g -DVK_USE_PLATFORM_XCB_KHR" % (in_filename, out_filename))
+
+    if compile_error != 0:
+        exit("\nbuild.py exited: compilation error")
+
+    built_objects.append(out_filename)
+
+
+linker_input_str = " ".join(built_objects)
+linker_error = os.system("clang %s -o zgae -lxcb -lvulkan" % linker_input_str)
+
+if linker_error != 0:
+    exit("\nbuild.py exited: linker error")
+
+if len(sys.argv) > 1 and sys.argv[1] == "run":
     os.system("./zgae")
